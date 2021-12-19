db6d952a2236f1b193dadfa80666f53113a2f2ae
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2021-12-19 12:27:20 +0000

    add travis image

--- cmd/gopher-boy/main.go
+++ cmd/gopher-boy/main.go
@@ -11,4 +11,13 @@ import (
 	"github.com/bokuweb/gopher-boy/pkg/pad"
 
 	"github.com/bokuweb/gopher-boy/pkg/gpu"
-	"github.com/bokuweb/
+	"github.com/bokuweb/gopher-boy/pkg/timer"
+	"github.com/bokuweb/gopher-boy/pkg/utils"
+
+	"github.com/bokuweb/gopher-boy/pkg/cpu"
+	"github.com/bokuweb/gopher-boy/pkg/gb"
+	"github.com/bokuweb/gopher-boy/pkg/logger"
+	"github.com/bokuweb/gopher-boy/pkg/ram"
+
+	"github.com/bokuweb/gopher-boy/pkg/bus"
+	"github.com/b
