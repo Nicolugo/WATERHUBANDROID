f2b00813f60edf8d45c6132061701705189de470
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-02-13 15:20:21 +0000

    Setting version to 0.1.1-SNAPSHOT

---
+++ docs/debugger/index.js
@@ -0,0 +1,8 @@
+const getPalette = c => {
+  switch (c) {
+    case 0:
+      return [175, 197, 160, 255];
+    case 1:
+      return [93, 147, 66, 255];
+    case 2:
+      return [22, 63, 48, 255]
