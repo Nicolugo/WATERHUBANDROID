9e3339643278be1e60b798b337ae1380c0c8e71c
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-02-18 22:00:40 +0000

    seccomp: ignore secure_computing return values

--- docs/debugger/index.js
+++ docs/debugger/index.js
@@ -5,4 +5,14 @@ const getPalette = c => {
     case 1:
       return [93, 147, 66, 255];
     case 2:
-      return [22, 63, 48, 255]
+      return [22, 63, 48, 255];
+    case 3:
+      return [0, 40, 0, 255];
+  }
+};
+
+const gpuLCDC = document.querySelector(".gpu-lcdc");
+const gpuSTAT = document.querySelector(".gpu-stat");
+const gpuSCROLLY = document.querySelector(".gpu-scrolly");
+const gpuSCROLLX = document.querySelector(".gpu-scrollx");
+const gpu
