60159b54f58b8c6722ec8382f249410103a9e65d
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-06-08 20:36:05 +0000

    Workflow

--- docs/index.js
+++ docs/index.js
@@ -14,4 +14,16 @@ window.onload = async () => {
   const go = new Go();
   const res = await fetch("./main.wasm");
   const bytes = await res.arrayBuffer();
-  const { instance } = await WebAssembly.instantiate(bytes, go.importObje
+  const { instance } = await WebAssembly.instantiate(bytes, go.importObject);
+  go.run(instance);
+
+  const canvas = document.querySelector(".game");
+  const input = document.querySelector("#file_upload");
+
+  // GPU
+
+  const init = async buf => {
+    const ctx = canvas.getContext("2d");
+    const image = ctx.createImageData(160, 144);
+
+    if (!buf
