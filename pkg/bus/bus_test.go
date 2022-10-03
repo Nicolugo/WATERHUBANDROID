bd3f341dc0fd728eb5d9dfb8010e9c64eeed83e4
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-03 10:08:17 +0000

    Update readme.

--- pkg/bus/bus_test.go
+++ pkg/bus/bus_test.go
@@ -10,4 +10,14 @@ import (
 	"github.com/bokuweb/gopher-boy/pkg/pad"
 	"github.com/bokuweb/gopher-boy/pkg/ram"
 	"github.com/bokuweb/gopher-boy/pkg/timer"
-	"github.com/bokuweb/goph
+	"github.com/bokuweb/gopher-boy/pkg/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func setup() (*Bus, *ram.RAM, *ram.RAM) {
+	buf := make([]byte, 0x8000)
+	cart, _ := cartridge.NewCartridge(buf)
+	vRAM := ram.NewRAM(0x2000)
+	wRAM := ram.NewRAM(0x2000)
+	hRAM := ram.NewRAM(0x80)
+	oamRAM := 
