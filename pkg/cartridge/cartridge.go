4018dac81d4f306d4f9966b03cf347d5ff9cca8d
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-19 13:05:54 +0000

    first add

--- pkg/cartridge/cartridge.go
+++ pkg/cartridge/cartridge.go
@@ -7,4 +7,21 @@ import (
 	"github.com/bokuweb/gopher-boy/pkg/types"
 )
 
-// Cartridge is GameBoy cartrid
+// Cartridge is GameBoy cartridge
+type Cartridge struct {
+	mbc     MBC
+	Title   string
+	ROM     []byte
+	RAMSize int
+}
+
+/*
+  CartridgeType is
+  0x00: ROM ONLY
+  0x01: ROM+MBC1
+  0x02: ROM+MBC1+RAM
+  0x03: ROM+MBC1+RAM+BATT
+  0x05: ROM+MBC2
+  0x06: ROM+MBC2+BATTERY
+  0x08: ROM+RAM
+  0x09: ROM+RAM
