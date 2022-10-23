d41eab1c64bc71167482cd5522d460fbd2ecc688
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-23 15:08:42 +0000

    correction readme

--- pkg/cartridge/mbc1.go
+++ pkg/cartridge/mbc1.go
@@ -9,4 +9,17 @@ import (
 // MBC1 is (Memory Bank Controller 1
 // MBC1 has two different maximum memory modes: 16Mbit ROM/8KByte RAM or 4Mbit ROM/32KByte RAM.
 type MBC1 struct {
-	rom             *r
+	rom             *rom.ROM
+	ram             *ram.RAM
+	selectedROMBank int
+	selectedRAMBank int
+	romBanking      bool
+	ramEnabled      bool
+	hasBattery      bool
+	memoryMode      MBC1MemoryMode
+	RAMSize         int
+}
+
+// MBC1MemoryMode is MBC1 max memory mode
+// The MBC1 defaults to 16Mbit ROM/8KByte RAM mode
+// on power up. Writing
