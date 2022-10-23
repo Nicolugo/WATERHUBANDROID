6eb5e849888b2640d4f7deb12b46a229b3665df5
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-23 12:41:54 +0000

    Updated readme [ci skip]

--- pkg/cartridge/mbc0.go
+++ pkg/cartridge/mbc0.go
@@ -2,4 +2,20 @@ package cartridge
 
 import (
 	"github.com/bokuweb/gopher-boy/pkg/rom"
-	"github.com/bokuweb/gopher-b
+	"github.com/bokuweb/gopher-boy/pkg/types"
+)
+
+// MBC0 ROM ONLY
+// This is a 32kB (256kb) ROM and occupies 0000-7FFF.
+type MBC0 struct {
+	rom *rom.ROM
+}
+
+// NewMBC0 constracts MBC0
+func NewMBC0(data []byte) *MBC0 {
+	m := new(MBC0)
+	m.rom = rom.NewROM(data)
+	return m
+}
+
+func (m *MBC0) Write(
