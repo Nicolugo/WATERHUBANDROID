f97502b371c27ec36177737989bec8d0bf9a792f
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-23 14:26:58 +0000

    Cleanup Makefile

--- pkg/cartridge/mbc1.go
+++ pkg/cartridge/mbc1.go
@@ -1,4 +1,12 @@
 package cartridge
 
 import (
-	"github.com/bokuweb/gopher-boy/pkg/ram
+	"github.com/bokuweb/gopher-boy/pkg/ram"
+	"github.com/bokuweb/gopher-boy/pkg/rom"
+	"github.com/bokuweb/gopher-boy/pkg/types"
+)
+
+// MBC1 is (Memory Bank Controller 1
+// MBC1 has two different maximum memory modes: 16Mbit ROM/8KByte RAM or 4Mbit ROM/32KByte RAM.
+type MBC1 struct {
+	rom             *r
