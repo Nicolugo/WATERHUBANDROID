1940b751e11db5f4f2df89678e1a4e3b7b6042d2
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-23 09:33:48 +0000

    debug cosmetic

--- pkg/cartridge/mbc.go
+++ pkg/cartridge/mbc.go
@@ -1,4 +1,12 @@
 package cartridge
 
 import (
-	"github.com/bokuweb/gop
+	"github.com/bokuweb/gopher-boy/pkg/types"
+)
+
+type MBC interface {
+	Write(addr types.Word, value byte)
+	Read(addr types.Word) byte
+	switchROMBank(bank int)
+	switchRAMBank(bank int)
+}
---
+++ pkg/cartridge/mbc0.go
@@ -0,0 +1,5 @@
+package cartridge
+
+import (
+	"github.com/bokuweb/gopher-boy/pkg/rom"
+	"github.com/bokuweb/gopher-b
