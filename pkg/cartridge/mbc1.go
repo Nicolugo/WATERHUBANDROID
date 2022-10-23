e20c64eb86cfb93dc6644b6ad4aeb7d6a8102596
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-23 15:59:55 +0000

    update helpers

--- pkg/cartridge/mbc1.go
+++ pkg/cartridge/mbc1.go
@@ -22,4 +22,11 @@ type MBC1 struct {
 
 // MBC1MemoryMode is MBC1 max memory mode
 // The MBC1 defaults to 16Mbit ROM/8KByte RAM mode
-// on power up. Writing
+// on power up. Writing a value (XXXXXXXS - X = Don't care, S = Memory model select) into 6000-7FFF area
+// will select the memory model to use.
+// S = 0 selects 16/8 mode. S = 1 selects 4/32 mode.
+type MBC1MemoryMode = string
+
+const (
+	// ROM16mRAM8kMode is 4/32 memory mode
+	// Writing a value (XXXXXXBB - X = Don'
