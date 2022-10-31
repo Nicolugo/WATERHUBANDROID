6428a8c3fccdaef4512544f4592f60642e7ee279
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-10-31 07:02:35 +0000

    updated .gitattributes

--- pkg/gb/gb.go
+++ pkg/gb/gb.go
@@ -7,4 +7,17 @@ import (
 	"github.com/bokuweb/gopher-boy/pkg/gpu"
 	"github.com/bokuweb/gopher-boy/pkg/interfaces/window"
 	"github.com/bokuweb/gopher-boy/pkg/interrupt"
-	"github.com/bokuweb/gopher-boy/pkg/tim
+	"github.com/bokuweb/gopher-boy/pkg/timer"
+)
+
+// CyclesPerFrame is cpu clock num for 1frame.
+const CyclesPerFrame = 70224
+
+// GB is gameboy emulator struct
+type GB struct {
+	currentCycle uint
+	cpu          *cpu.CPU
+	gpu          *gpu.GPU
+	timer        *timer.Timer
+	irq          *interrupt.Interrupt
+	
