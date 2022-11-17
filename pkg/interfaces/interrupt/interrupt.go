bd973802d3e6da60ef431e922a26468550bff97f
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-17 14:14:47 +0000

    Added MIT license

--- pkg/interfaces/interrupt/interrupt.go
+++ pkg/interfaces/interrupt/interrupt.go
@@ -6,4 +6,16 @@ import (
 )
 
 // IRQFlag is
-type IRQFlag = interrupt
+type IRQFlag = interrupt.IRQFlag
+
+// Interrupt defined irq interface
+type Interrupt interface {
+	SetIRQ(f interrupt.IRQFlag)
+	Enable()
+	Disable()
+	Enabled() bool
+	Read(addr types.Word) byte
+	Write(addr types.Word, data byte)
+	HasIRQ() bool
+	ResolveISRAddr() *types.Word
+}
---
+++ pkg/interfaces/logger/logger.go
@@ -0,0 +1,10 @@
+
+package logger
+
+// Logger is
+type Logger interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Error(args ...interface{})
+	Warn(args ...interface{})
+}
