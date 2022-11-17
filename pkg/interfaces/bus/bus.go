8e389899147ca57e37cfccbce6ff125204f0d078
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-17 09:31:48 +0000

    Minor refactoring

--- pkg/interfaces/bus/bus.go
+++ pkg/interfaces/bus/bus.go
@@ -5,4 +5,8 @@ import "github.com/bokuweb/gopher-boy/pkg/types"
 // Accessor bus accessor interface
 type Accessor interface {
 	WriteByte(addr types.Word, data byte)
-	WriteWord(addr types
+	WriteWord(addr types.Word, data types.Word)
+
+	ReadByte(addr types.Word) byte
+	ReadWord(addr types.Word) types.Word
+}
---
+++ pkg/interfaces/interrupt/interrupt.go
@@ -0,0 +1,9 @@
+package interrupt
+
+import (
+	"github.com/bokuweb/gopher-boy/pkg/interrupt"
+	"github.com/bokuweb/gopher-boy/pkg/types"
+)
+
+// IRQFlag is
+type IRQFlag = interrupt
