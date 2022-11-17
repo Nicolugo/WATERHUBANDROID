8f02218ac4fcb9a2461a77590b5431410f0ff58a
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-17 19:46:01 +0000

    cleanup and docs

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -4,4 +4,22 @@ import (
 	"log"
 )
 
-// LogLevel 
+// LogLevel is kind of Loglavel
+type LogLevel string
+
+const (
+	// LogDebug is
+	LogDebug LogLevel = "Debug"
+	// LogInfo is
+	LogInfo LogLevel = "Info"
+	// LogSilent is
+	LogSilent LogLevel = "Silent"
+)
+
+// Log is
+type Log struct {
+	Level LogLevel
+}
+
+// NewLogger is logger constructor
+func NewLog
