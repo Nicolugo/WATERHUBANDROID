ac4c0b9a8e41e22b4a2d89ca6774833ea36d8c92
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-21 17:43:40 +0000

    Code Review

--- pkg/mocks/bus.go
+++ pkg/mocks/bus.go
@@ -13,4 +13,14 @@ func (b *MockBus) WriteByte(addr types.Word, data byte) {
 }
 
 func (b *MockBus) WriteWord(addr types.Word, data types.Word) {
-	b.MockMemory[addr] = b
+	b.MockMemory[addr] = byte(data & 0xFF)
+	b.MockMemory[addr+1] = byte(data >> 8)
+}
+
+func (b *MockBus) ReadByte(addr types.Word) byte {
+	return b.MockMemory[addr]
+}
+
+func (b *MockBus) ReadWord(addr types.Word) types.Word {
+	upper := types.Word(b.MockMemory[addr]) << 8
+	return upper + types.Word(b
