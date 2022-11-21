feef7e119d2e9ace72a2d4f3a8067df53a19aae2
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-21 10:08:51 +0000

    line ending

--- pkg/mocks/bus.go
+++ pkg/mocks/bus.go
@@ -1,3 +1,16 @@
 package mocks
 
-i
+import (
+	"github.com/bokuweb/gopher-boy/pkg/types"
+)
+
+type MockBus struct {
+	MockMemory [0x10000]byte
+}
+
+func (b *MockBus) WriteByte(addr types.Word, data byte) {
+	b.MockMemory[addr] = data
+}
+
+func (b *MockBus) WriteWord(addr types.Word, data types.Word) {
+	b.MockMemory[addr] = b
