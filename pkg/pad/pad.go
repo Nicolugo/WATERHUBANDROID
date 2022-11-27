c9441a2c43cd2ca7eda4c9d56abe7566105900bb
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-27 07:13:19 +0000

    Update config files.

--- pkg/pad/pad.go
+++ pkg/pad/pad.go
@@ -14,4 +14,19 @@ package pad
 
 type Pad struct {
 	// Bit 7 - Not used
-	//
+	// Bit 6 - Not used
+	// Bit 5 - P15 out port
+	// Bit 4 - P14 out port
+	// Bit 3 - P13 in port
+	// Bit 2 - P12 in port
+	// Bit 1 - P11 in port
+	// Bit 0 - P10 in port
+	reg   byte
+	state Button
+}
+
+type Button byte
+
+const (
+	// A is the A button on the GameBoy.
+	A Button = 
