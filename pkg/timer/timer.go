3995185671dfa1616b11a00964635478d292e385
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-27 18:42:40 +0000

    role

--- pkg/timer/timer.go
+++ pkg/timer/timer.go
@@ -8,4 +8,9 @@ const (
 	// TimerRegisterOffset is register offset address
 	TimerRegisterOffset types.Word = 0xFF00
 	// DIV - Divider Register (R/W)
-	// 
+	// This register is incremented 16384 (~16779 on SGB) times a second.
+	// Writing any value sets it to $00.
+	DIV = 0x04
+	// TIMA - Timer counter (R/W)
+	// This timer is incremented by a clock frequency specified by the TAC register ($FF07).
+	// The timer generates an interrupt when it overfl
