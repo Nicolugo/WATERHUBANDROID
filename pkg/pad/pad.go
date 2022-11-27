bee0506c552250989d681f207ad8674c7d5a5dd1
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-11-27 06:15:20 +0000

    update api doc

--- pkg/pad/pad.go
+++ pkg/pad/pad.go
@@ -2,4 +2,16 @@ package pad
 
 // This is the matrix layout for register $FF00:
 //              P14      P15
-/
+//               |        |
+//  P10-------O-Right----O-A
+//               |        |
+//  P11-------O-Left-----O-B
+//               |        |
+//  P12-------O-Up-------O-Select
+//              |         |
+//  P13-------O-Down-----O-Start
+//
+
+type Pad struct {
+	// Bit 7 - Not used
+	//
