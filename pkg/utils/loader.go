a03dad234edf27e5ed708aec448438ad2964c75b
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2022-12-02 12:57:59 +0000

    url updated

--- pkg/utils/loader.go
+++ pkg/utils/loader.go
@@ -7,4 +7,15 @@ import (
 
 // LoadROM loads gameboy ROMfile to buf
 func LoadROM(filename string) ([]byte, error) {
-	file, err 
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	stats, statsErr := file.Stat()
+	if statsErr != nil {
+		return nil, statsErr
+	}
+	size := stats.Size()
+	bytes := make([]byte, size)
+	b := bufio.NewReader(file)
