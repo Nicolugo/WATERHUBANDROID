d1d3173816bc70416d50b68b8929ab23d9376795
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-01 20:23:41 +0000

    add my name

--- roms/instr_timing/source/common/build_rom.s
+++ roms/instr_timing/source/common/build_rom.s
@@ -19,4 +19,12 @@
 ; GB header read by bootrom
 .org $100
      nop
-   
+     jp   reset
+
+; Nintendo logo required for proper boot
+.byte $CE,$ED,$66,$66,$CC,$0D,$00,$0B
+.byte $03,$73,$00,$83,$00,$0C,$00,$0D
+.byte $00,$08,$11,$1F,$88,$89,$00,$0E
+.byte $DC,$CC,$6E,$E6,$DD,$DD,$D9,$99
+.byte $BB,$BB,$67,$63,$6E,$0E,$EC,$CC
+.byte $DD,$DC,$99,$9F,$BB,$B
