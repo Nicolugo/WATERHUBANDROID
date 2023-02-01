ff6df3b8d8afabfafb693cf149badffccf9a8945
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-01 16:30:58 +0000

    linted code

--- roms/helloworld/make.cmd
+++ roms/helloworld/make.cmd
@@ -1,3 +1,5 @@
 @echo off
 cls
-rgbasm -ohello.o
+rgbasm -ohello.o hello.s
+rgblink -p00 -ohello.gb hello.o
+rgbfix -v -m00 -p00 -tHello hello.gb 
---
+++ roms/instr_timing/source/common/build_rom.s
@@ -0,0 +1,5 @@
+; Build as GB ROM
+
+.memoryMap
+     defaultSlot 0
+     slot 0 $0000 
