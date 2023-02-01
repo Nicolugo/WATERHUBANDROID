bd40c2cd916c6586332330889049d7e72bb4f258
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-01 18:15:42 +0000

    Removed unneeded stuff

--- roms/instr_timing/source/common/build_rom.s
+++ roms/instr_timing/source/common/build_rom.s
@@ -2,4 +2,21 @@
 
 .memoryMap
      defaultSlot 0
-     slot 0 $0000 
+     slot 0 $0000 size $4000
+     slot 1 $C000 size $4000
+.endMe
+
+.romBankSize   $4000 ; generates $8000 byte ROM
+.romBanks      2
+
+.cartridgeType 1 ; MBC1
+.computeChecksum
+.computeComplementCheck
+
+
+;;;; GB ROM header
+
+; GB header read by bootrom
+.org $100
+     nop
+   
