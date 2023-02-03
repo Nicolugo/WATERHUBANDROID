7993a332f3b8c1e16bccbc54787682d38183ee82
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-03 08:11:48 +0000

    Fixed namespaces

--- roms/instr_timing/source/common/crc.s
+++ roms/instr_timing/source/common/crc.s
@@ -1,3 +1,17 @@
 ; CRC-32 checksum calculation
 
-.defi
+.define checksum    dp+0 ; little-endian, complemented
+.redefine dp        dp+4
+
+
+; Initializes checksum module. Might initialize tables
+; in the future.
+init_crc:
+     jr   reset_crc
+
+
+; Clears CRC
+; Preserved: BC, DE, HL
+reset_crc:
+     ld   a,$FF
+     sta  checksu
