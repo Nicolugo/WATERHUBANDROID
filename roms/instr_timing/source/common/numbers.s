c4790fdc4215994f0fcaf8c0a24a96275a7eebcd
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-09 16:47:12 +0000

    remove unused dep

--- roms/instr_timing/source/common/numbers.s
+++ roms/instr_timing/source/common/numbers.s
@@ -2,4 +2,20 @@
 
 ; Prints value of indicated register/pair
 ; as 2/4 hex digits, followed by a space.
-; Updates checksum with printed 
+; Updates checksum with printed values.
+; Preserved: AF, BC, DE, HL
+
+print_regs:
+     call print_af
+     call print_bc
+     call print_de
+     call print_hl
+     call print_newline
+     ret
+
+print_a:
+     push af
+print_a_:
+     call print_hex
+     ld   a,' '
+     call print_ch
