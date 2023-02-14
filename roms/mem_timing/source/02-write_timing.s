8cb67b94612929b68a5cdfa7c104165d2043ae5d
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-14 22:50:24 +0000

    remove grunt

--- roms/mem_timing/source/02-write_timing.s
+++ roms/mem_timing/source/02-write_timing.s
@@ -6,4 +6,11 @@
 
 instructions:
      ; last value is time of write
-     .byte $36,$FF,$0
+     .byte $36,$FF,$00,3 ; LD   (HL),n
+     .byte $70,$00,$00,2 ; LD   (HL),B
+     .byte $71,$00,$00,2 ; LD   (HL),C
+     .byte $72,$00,$00,2 ; LD   (HL),D
+     .byte $73,$00,$00,2 ; LD   (HL),E
+     .byte $74,$00,$00,2 ; LD   (HL),H
+     .byte $75,$00,$00,2 ; LD   (HL),L
+     
