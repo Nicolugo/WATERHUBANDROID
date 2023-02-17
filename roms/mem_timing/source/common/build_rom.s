e85d1cdb7c3fa2df5192fd8d404e77ae1c77f001
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-17 16:14:59 +0000

    Updated imap (markdown)

--- roms/mem_timing/source/common/build_rom.s
+++ roms/mem_timing/source/common/build_rom.s
@@ -22,4 +22,16 @@
      jp   reset
 
 ; Nintendo logo required for proper boot
-.byte $CE,$ED,$66,$66,$
+.byte $CE,$ED,$66,$66,$CC,$0D,$00,$0B
+.byte $03,$73,$00,$83,$00,$0C,$00,$0D
+.byte $00,$08,$11,$1F,$88,$89,$00,$0E
+.byte $DC,$CC,$6E,$E6,$DD,$DD,$D9,$99
+.byte $BB,$BB,$67,$63,$6E,$0E,$EC,$CC
+.byte $DD,$DC,$99,$9F,$BB,$B9,$33,$3E
+
+; Internal name
+.ifdef ROM_NAME
+     .byte ROM_NAME
+.endif
+
+; CGB/DMG requireme
