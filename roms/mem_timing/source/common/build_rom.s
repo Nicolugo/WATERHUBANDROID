028a7265b75ea9577f340a41030e9fdf9fc97f5d
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-17 13:54:25 +0000

    tweaks to README

--- roms/mem_timing/source/common/build_rom.s
+++ roms/mem_timing/source/common/build_rom.s
@@ -6,4 +6,20 @@
      slot 1 $C000 size $4000
 .endMe
 
-.rom
+.romBankSize   $4000 ; generates $8000 byte ROM
+.romBanks      2
+
+.cartridgeType 1 ; MBC1
+.computeChecksum
+.computeComplementCheck
+
+
+;;;; GB ROM header
+
+; GB header read by bootrom
+.org $100
+     nop
+     jp   reset
+
+; Nintendo logo required for proper boot
+.byte $CE,$ED,$66,$66,$
