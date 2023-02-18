1cbe1baf05cb28cd8dc933c27597249d14d71cf7
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-18 09:31:15 +0000

    Updated the pyiron_base feedstock.

--- roms/mem_timing/source/common/printing.s
+++ roms/mem_timing/source/common/printing.s
@@ -1,4 +1,18 @@
 ; Main printing routine that checksums and
 ; prints to output device
 
-; Character that does equivalent o
+; Character that does equivalent of print_newline
+.define newline 10
+
+; Prints char without updating checksum
+; Preserved: BC, DE, HL
+.define print_char_nocrc bss
+.redefine bss            bss+3
+
+
+; Initializes printing. HL = print routine
+init_printing:
+     ld   a,l
+     ld   (print_char_nocrc+1),a
+     ld   a,h
+     ld   (print
