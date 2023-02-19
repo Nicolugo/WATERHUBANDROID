245dd0e02c5c40edc930e061dc4f2f1c65c43df2
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-19 08:19:15 +0000

    5.6.1

--- roms/mem_timing/source/common/testing.s
+++ roms/mem_timing/source/common/testing.s
@@ -13,4 +13,23 @@
      jr   @set_test\@
      .byte \1
      .if NARGS > 1
-     
+          .byte \2
+     .endif
+     .if NARGS > 2
+          .byte \3
+     .endif
+     .byte 0
+@set_test\@:
+     pop  hl
+.endm
+
+set_test_:
+     pop  hl
+     push hl
+     push af
+     inc  hl
+     inc  hl
+     ldi  a,(hl)
+     ld   (result),a
+     ld   a,l
+     ld   (test_
