18193d2b865f0e7f62bdbb2d8042967c40d19839
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-19 08:19:00 +0000

    update doc strings

--- roms/mem_timing/source/common/testing.s
+++ roms/mem_timing/source/common/testing.s
@@ -1,3 +1,16 @@
 ; Diagnostic and testing utilities
 
-.define result
+.define result      bss+0
+.define test_name   bss+1
+.redefine bss       bss+3
+
+
+; Sets test code and optional error text
+; Preserved: AF, BC, DE, HL
+.macro set_test ; code[,text[,text2]]
+     push hl
+     call set_test_
+     jr   @set_test\@
+     .byte \1
+     .if NARGS > 1
+     
