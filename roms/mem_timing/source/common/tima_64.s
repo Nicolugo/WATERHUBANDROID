c4ef7f69d40d681111b7da28ccb88c34cda65765
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-02-23 05:37:32 +0000

    rtc-cmos: Add an alarm disable quirk

--- roms/mem_timing/source/common/tima_64.s
+++ roms/mem_timing/source/common/tima_64.s
@@ -1,3 +1,24 @@
 ; Timer that's incremented every 64 cycles
 
-; Initial
+; Initializes timer for use by sync_tima_64
+init_tima_64:
+     wreg TMA,0
+     wreg TAC,$07
+     ret
+
+; Synchronizes to timer
+sync_tima_64:
+     push af
+     push hl
+     
+     ld   a,0
+     ld   hl,TIMA
+     ld   (hl),a
+-    or   (hl)
+     jr   z,-
+     
+-    delay 65-12
+     xor  a
+     ld   (hl),a
+     or   (hl)
+     dela
